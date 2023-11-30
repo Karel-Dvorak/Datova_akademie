08d6cd5404023f4319202b21b342dbda6b2c6c74
Karel <dvorak.karl@seznam.cz>
2023-11-30 19:18:46 +0100

    Odpovědi k 1. otázce

---
+++ projekt_1/1.question.sql
@@ -0,0 +1,59 @@
+-- Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+
+
+-- vytvoření pohledu
+CREATE OR REPLACE VIEW v_karel_dvorak_first_question AS
+WITH base AS (
+	SELECT 
+		DISTINCT date,
+		industry_name,
+		avg_wages
+	FROM t_karel_dvorak_project_sql_primary_final t
+	ORDER BY industry_name, date
+), 
+lagged AS (
+	SELECT
+		date,
+		industry_name,
+		avg_wages,
+		LAG(avg_wages) OVER (ORDER BY industry_name, date) AS lag_wages
+	FROM base
+)
+SELECT 
+	*,
+	avg_wages - lag_wages AS diff,
+	CASE
+		WHEN avg_wages - lag_wages != 0 AND  date = '2006' THEN NULL	
+		WHEN avg_wages - lag_wages >=1 THEN 'Rising'
+		WHEN avg_wages - lag_wages < 1 tHEN 'Falling'
+	END AS rising_falling
+FROM lagged
+; 
+
+-- Mzdy klesají v těchto letech a odvětvých
+SELECT *
+FROM v_karel_dvorak_first_question vkdfq 
+WHERE rising_falling = 'Falling'
+;
+
+-- Největší propady mezd
+SELECT 
+	date,
+	industry_name,
+	diff,
+	rising_falling
+FROM v_karel_dvorak_first_question vkdfq
+WHERE rising_falling = 'Falling'
+ORDER BY diff ASC
+;
+
+-- Nejhorší roky v poklesu mezd
+SELECT 
+	date,
+	count(date) AS sum_date 
+FROM v_karel_dvorak_first_question vkdfq
+WHERE rising_falling = 'Falling'
+GROUP BY date
+ORDER BY count(date)DESC
+;
+
