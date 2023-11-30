ac73613115794c18c404278f576069f1d11146f7
Karel <dvorak.karl@seznam.cz>
2023-11-30 10:18:43 +0100

    dodatečná tabulka k projektu SQL

--- projekt_1/script_secondary_table.sql
+++ projekt_1/script_secondary_table.sql
@@ -1,12 +1,12 @@
 /* 
  * Jako dodatečný materiál připravte i tabulku s HDP, GINI koeficientem a populací 
  * dalších evropských států ve stejném období, jako primární přehled pro ČR.
+ *
+ * countries - Všemožné informace o zemích na světě, například hlavní město, měna, národní jídlo nebo průměrná výška populace.
+ * economies - HDP, GINI, daňová zátěž, atd. pro daný stát a rok.
+ * 
+ * t_{jmeno}_{prijmeni}_project_SQL_secondary_final (pro dodatečná data o dalších evropských státech).
  */
-
--- countries - Všemožné informace o zemích na světě, například hlavní město, měna, národní jídlo nebo průměrná výška populace.
--- economies - HDP, GINI, daňová zátěž, atd. pro daný stát a rok.
-
--- t_{jmeno}_{prijmeni}_project_SQL_secondary_final (pro dodatečná data o dalších evropských státech).
 
 CREATE OR REPLACE TABLE t_karel_dvorak_project_SQL_secondary_final
 SELECT 
